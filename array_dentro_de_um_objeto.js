86389265d78b71fe2bdb221ac975a61c700ee0a7
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-07 14:20:44 -0300

    add arquivos

---
+++ array_dentro_de_um_objeto.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: 'André',
+    idade: 36,
+    cpf: '12365242625',
+    email: 'andre@email.com',
+    fones: ['55912345498', '5521988743124']
+}
+cliente.fones.forEach(fone => console.log(fone));
