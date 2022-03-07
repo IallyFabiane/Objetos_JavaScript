4820736f0821cc9788aa34b2cb91de5f34b240c6
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-07 13:08:39 -0300

    ok

--- acessar_lista._com_colchetes.js
+++ acessar_lista._com_colchetes.js
@@ -7,3 +7,5 @@ const cliente = {
 
 const chaves = ['nome', 'idade', 'cpf', 'email']; // array com as chaves a serem acessadas
 console.log(cliente[chaves[0]]); // notação de colchetes
+chaves.forEach(info => console.log(cliente[info])); //acessando todo o array
+console.log(cliente['nome']); // usando exatamente o nome da chave
