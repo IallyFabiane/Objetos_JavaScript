b891e0b263eddd2b9809877988c58b7d518f8890
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-07 13:31:02 -0300

    add arquivos

---
+++ deletando_propriedades_em_um_objeto.js
@@ -0,0 +1,20 @@
+const objPersonagem = {
+    nome: "Gandalf",
+    classe: "mago",
+    nivel: "20",
+    aliado: {
+      nome: "Saruman",
+      classe: "mago"
+    },
+    status: "desaparecido"
+   }
+delete objPersonagem.aliado; // deletando a propriedade com notação de ponto
+console.log(objPersonagem.aliado);
+delete objPersonagem.aliado
+delete objPersonagem["status"]; // deletando a propriedade com notação de colchetes
+console.log(objPersonagem.status);
+
+const delProp = delete objPersonagem.aliado;
+const delPropInexistente = delete objPersonagem["endereco"];
+console.log(delProp); //true
+console.log(delPropInexistente); //true
