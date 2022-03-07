6a01ececf0a8e24e79770c189c19d1ca03b67515
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-07 13:16:40 -0300

    addicionado arquivos

---
+++ adicionando_campos-no-objeto.js
@@ -0,0 +1,11 @@
+const cliente = {
+    nome: 'André',
+    idade: 36,
+    cpf: '12365242625',
+    email: 'andre@email.com'
+}
+
+cliente.fone = '9695432353';
+console.log(cliente); // adicionando uma propriedade no objeto
+cliente.fone = '1235669874585';
+console.log(cliente); // substituindo o valor de uma propriedade já existente
