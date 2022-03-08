d9365fa79f18170589fee6c5415093e240d30372
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-08 16:19:10 -0300

    adicionando arquivo

---
+++ propriedades_enumeráveis.js
@@ -0,0 +1,12 @@
+const catalogo = {
+    "editora": "Casa do Código",
+    "catalogo": [
+    {
+      "id": 50,
+      "titulo": "Primeiros Passos com NodeJS",
+      "autor": "João Rubens",
+      "categoria": "programação",
+      "versoes": ["ebook", "impresso"]
+    }
+]}
+console.log(Object.getOwnPropertyDescriptor(catalogo, "editora")); // descrevendo os atributos da propriedade própria
