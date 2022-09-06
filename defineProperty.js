9939e161a33441e74edd75821e0e64cab5a23d3a
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-09-06 17:38:14 -0300

    Create defineProperty.js
    defineProperty

---
+++ defineProperty.js
@@ -0,0 +1,18 @@
+function Produto (nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, //mostra a chave
+        value: estoque, //valor atruibuído a propriedade é igual ao da instância
+        writable: true, // é alterável
+        configurable: false // não pode ser reconfigurado(apagado, por exemplo)
+    })
+}
+
+const produto = new Produto('camiseta', 50, 3);
+console.log(produto);
+console.log(Object.keys(produto));
+for (let chave in produto) {
+    console.log(chave);
+};
