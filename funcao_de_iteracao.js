176a5837421473e17686bb640860c5186b465135
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-10-10 12:24:58 -0300

    métodos some, every e forEach e seus efeitos na iteração de um array

---
+++ funcao_de_iteracao.js
@@ -0,0 +1,15 @@
+function isEven(x) {
+    console.log(x);
+    return x % 2 === 0 ? true : false;
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+
+//every itera pelos elementos do array até que a função devolva false!
+numbers.every(isEven); //o primeiro número do array é 1 então não é múltiplo de 2, logo isEven retorna false ao encontrar numbers[0]
+
+//some itera pelos elementos do array até que a função devolva true!
+numbers.some(isEven);//o segundo número do array é 2 então é múltiplo de 2, logo some retorna true ao encontrar numbers[1]
+
+numbers.forEach(x => console.log(x % 2 === 0));//itera sobre todos os elementos do array checando se são múltiplos de 2 ou não e retorna true pou false
