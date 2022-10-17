36f47cacd51130f59fe99aba4b499b0a9dffc944
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-10-17 15:04:02 -0300

    método fill()

---
+++ fill.js
@@ -0,0 +1,5 @@
+let ones = Array(6).fill(1); // o array ones terá 6 posições todas preenchidas com o argumento passado para fill
+console.log(ones);
+let numbers = [1, 2, 3, 4, 5, 6];
+numbers.fill(0, 2, 5);
+console.log(numbers);// o array numbers é preenchido com 0 (primeiro argumento) do índice 2 (segundo argumento) ao 5 (terceiro argumento)
