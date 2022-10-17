cbbf5b1f4c34364e336fcd8b933f89d1ce1db9d2
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-10-17 16:28:15 -0300

    método reverse()

---
+++ reverse.js
@@ -0,0 +1,3 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7,8 , 9, 10, 11, 12, 13, 14, 15];
+numbers.reverse(); // retorna um array com os valores na ordem inversa (o primeiro será o último e vice-versa)
+console.log(numbers);
