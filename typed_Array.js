9f78fe3ea68a1231d4a33a85bae32e18083549cf
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-10-18 12:29:15 -0300

    introdução ao conceito de Typed Arrays em JS

---
+++ typed_Array.js
@@ -0,0 +1,9 @@
+let length = 5;
+let int16 = new Int16Array(length); // utilizado para manipular dados binários
+let array16 = [];
+array16.length = length;
+for (let i=0; i<length; i++) {
+    int16[i] = i+1;
+};
+
+console.log(int16);
