4bc6df16f74253c39decfd27df65830b9d74e203
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-10-17 21:41:14 -0300

    método localeCompare()

---
+++ locale_compare.js
@@ -0,0 +1,3 @@
+let names = ['Ana', 'ana', 'John', 'john'];
+
+console.log(names.sort((a, b) => a.localeCompare(b))); //retorna um número que indica se uma string de referência vem antes ou depois, ou é a mesma que a string fornecida na ordem de classificação.
