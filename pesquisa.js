a064bfafc0fdf987a809c2b37351828c9d49b392
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-10-17 21:49:52 -0300

    métodos indexOf() e lastIndexOf()

---
+++ pesquisa.js
@@ -0,0 +1,4 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+console.log(numbers.indexOf(10)); // retorna o primeiro índice encontrado de acordo com o argumento passado como valor para o método
+console.log(numbers.lastIndexOf(15)); // retorna o último índice encontrado de acordo com o argumento passado como valor para o método
